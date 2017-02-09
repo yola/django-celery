e4f8fa96db0fce0c7daa084e99e9006a1c9a4675
Stefano Rivera <stefano@rivera.za.net>
2017-02-09 10:56:40 -0800

    Use + syntax for our fork

--- djcelery/__init__.py
+++ djcelery/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
-VERSION = (3, 1, 16, 'yola1')
+VERSION = (3, 1, 16, '+yola1')
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
